06e51554db4d73453f26dc9520f88241bb2a3768
Jordan Klotz <jordanklotz@Jordans-MacBook-Air.local>
2025-09-02 19:07:14 -0500

    added exercise 1 and 2

--- site.js
+++ site.js
@@ -1,24 +1,24 @@
 
-const activeElements = document.querySelectorAll('.active')
+//const activeElements = document.querySelectorAll('.active')
 // add border
 //activeElements.forEach(el => el.computedStyleMap.border = '1px solid red')
 
-for (let i = 0; i < activeElements.length; i++) 
+//for (let i = 0; i < activeElements.length; i++) 
 {
-    activeElements[i].style.border = '1px solid red';
+    //activeElements[i].style.border = '1px solid red';
 }
 
 //const footer = document.querySelector('footer');
 //const selectLink = document.querySelector('footer a');
 //selectLink.style.textDecoration = 'underline';
 
-const selectLink = document.querySelector('#link');
-selectLink.style.textDecoration = 'underline';
+//const selectLink = document.querySelector('#link');
+//selectLink.style.textDecoration = 'underline';
 
 //do something to the body
 //document.body.style.backgroundColor ='red';
 
-const contact = document.querySelector('#contact')
+//const contact = document.querySelector('#contact')
 
 /*contact.addEventListener('mouseover', () => {
     contact.parentElement.style.backgroundColor = 'lightblue' 
@@ -27,3 +27,12 @@ const contact = document.querySelector('#contact')
 /*contact.addEventListener('mouseover', function() {
     contact.closest('nav').style.backgroundColor = 'lightblue' 
 })*/
+
+const mainContentSection = document.querySelector('h2')
+mainContentSection.style.color = 'red';
+
+const link = document.querySelector('footer a') // select the anchor in the footer
+link.style.textDecoration = 'underline'; // underline the link
+
+const link = document.querySelector('#link')// select the anchor with id of link
+link.style.textDecoration = 'underline'; // underline the link
